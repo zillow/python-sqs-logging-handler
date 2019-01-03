ad13709760a2581a842b6acfdbefde130e6dfd1d
Martin Charrel <martin.charrel@datadoghq.com>
2019-01-03 14:13:20 +0100

    Add kwarg to use IAM role instead of keys

--- sqs_log_handler/sqsloghandler.py
+++ sqs_log_handler/sqsloghandler.py
@@ -7,7 +7,7 @@ from retrying import retry
 class SQSHandler(logging.Handler):
     """ A Python logging handler which sends messages to Amazon SQS. """
 
-    def __init__(self, queue, aws_key_id=None, secret_key=None, aws_region=None, global_extra=None):
+    def __init__(self, queue, aws_key_id=None, secret_key=None, aws_region=None, global_extra=None, use_iam_role=False):
         """
         Sends log messages to SQS so downstream processors can consume
         (e.g. push the log messages to Splunk).
@@ -18,10 +18,13 @@ class SQSHandler(logging.Handler):
         """
 
         logging.Handler.__init__(self)
-        client = boto3.resource('sqs',
-                                aws_access_key_id=aws_key_id,
-                                aws_secret_access_key=secret_key,
-                                region_name=aws_region)
+        if use_iam_role:
+            client = boto3.resource('sqs', region_name=aws_region)
+        else:
+            client = boto3.resource('sqs',
+                                    aws_access_key_id=aws_key_id,
+                                    aws_secret_access_key=secret_key,
+                                    region_name=aws_region)
         self.queue = client.get_queue_by_name(QueueName=queue)
         self._global_extra = global_extra
 
