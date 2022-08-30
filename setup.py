8e89f5297b817ab5e1825ddcb00b299a3b5528bd
Francisco Gray <francisco.gray@instructure.com>
2022-08-30 16:12:45 -0700

    Bumps version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def read_text_file(fname):
 
 setup(
     name="sqs-log-handler",
-    version="1.1.0",
+    version="1.1.1",
     author="Wei Liu",
     author_email="weil@zillowgroup.com",
     description="a python log handler that pushes logs into AWS sqs",
